7d024a024875efe2004d3a32af7ae285c4099048
Gleb-Kolonin <topsecretway@yandex.ru>
2022-08-05 07:59:11 +0300

    Валидация данных с помощью pydantic и typing

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 
+# Импорт моделей pydantic для валидации данных
+from schemas import Book
 
 app = FastAPI()
 
@@ -9,10 +11,17 @@ def home():
     return {"key": "Hello"}
 
 # Обработчик get запроса с параметром
-@app.get('/{pk}')
+@app.get('/{pk}') # Пример: http://127.0.0.1:8000/5?q=22
 def get_item(pk: int, q: str = None): #fastAPI проведёт валидацию типов данных, поэтому указание типа данных - полезно
     return {"key": pk, "q": q}
 
+@app.get('/user/{pk}/items/{item}/')
+def get_user_item(pk: int, item: str):
+    return {"key": pk, "item": item}
+
+@app.post('/book') # POST - Для публикации данных на сервер
+def create_book(item: Book):
+    return item
 
 '''
 Запуск сервера:
---
+++ schemas.py
@@ -0,0 +1,17 @@
+# Файл с pydantic моделями для валидации данных, отправляемых на сервер
+from typing import List # Для аннотирования типов данных. Как строгая типизация
+
+from pydantic import BaseModel
+from datetime import date
+
+class Genre(BaseModel):
+    name: str
+
+class Book(BaseModel):
+    title: str
+    writer: str
+    duration: str
+    date: date
+    summary: str
+    genres: List[Genre] # Список из жанров, что разрешены моделью Genre.
+
