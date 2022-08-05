0d499402c12da1d1a2f25793c4843010e3c75620
Gleb-Kolonin <topsecretway@yandex.ru>
2022-08-05 08:45:28 +0300

    8 Валидация полей с помощью pydantic Field

--- schemas.py
+++ schemas.py
@@ -1,24 +1,26 @@
 # Файл с pydantic моделями для валидации данных, отправляемых на сервер
 from typing import List # Для аннотирования типов данных. Как строгая типизация
 
-from pydantic import BaseModel, validator
+from pydantic import BaseModel, validator, Field
 from datetime import date
 
 class Genre(BaseModel):
     name: str
 
 class Author(BaseModel):
-    first_name: str
+    first_name: str = Field(..., max_length=25)
     last_name: str
-    age: int
+    age: int = Field(..., gt=15, lt=90, description="Возраст должен быть > 15 и < 90") # Валидация через Field
 
     # Валидатор для поля pydantic модели
     # Позволяет гарантировать создание авторов определённого возраста
+    '''
     @validator('age')
     def check_age(cls, v):
         if v <= 18:
             raise ValueError("Автор должен быть старше 18")
         return v
+    '''
 class Book(BaseModel):
     title: str
     writer: str
@@ -27,3 +29,4 @@ class Book(BaseModel):
     summary: str
     genres: List[Genre] # Список из жанров, что разрешены моделью Genre.
     pages: int
+
